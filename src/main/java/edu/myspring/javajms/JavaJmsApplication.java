db728f0e27dcd033e10c72ed086229d03271794c
dpudovkin <dpudovkin@sbrf.ru>
2022-04-11 19:11:47 +0300

    Initial commit

---
+++ src/main/java/edu/myspring/javajms/JavaJmsApplication.java
@@ -0,0 +1,13 @@
+package edu.myspring.javajms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/myspring/javajms/JavaJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.myspring.javajms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
